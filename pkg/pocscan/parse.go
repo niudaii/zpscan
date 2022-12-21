72fd5f0cd45e05a4b3d45479f1f1d3453724e7ef
niudaii <243536998@qq.com>
2022-12-21 11:00:05 +0800

    update: expscan

--- pkg/pocscan/parse.go
+++ pkg/pocscan/parse.go
@@ -29,7 +29,7 @@ func ParsePocInput(targets []string) (results []*PocInput, err error) {
 func ParseExpInput(targets []string, payload string) (results []*ExpInput, err error) {
 	for _, target := range targets {
 		if !strings.Contains(target, "|") {
-			err = fmt.Errorf("input 必须指定 pocTags，example: -i http://127.0.0.1:9200|elasticsearch")
+			err = fmt.Errorf("input 必须指定 pocTags，example: -i http://127.0.0.1:9200|CVE-2015-1427")
 			return
 		}
 		target = strings.TrimSpace(target)
