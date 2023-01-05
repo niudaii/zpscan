45ada18dea399910a82e99db26258e5ddfbfae9f
niudaii <243536998@qq.com>
2023-01-05 17:54:12 +0800

    feat: alive

--- pkg/domainscan/domainweb/domainweb_test.go
+++ pkg/domainscan/domainweb/domainweb_test.go
@@ -3,7 +3,7 @@ package domainweb
 import "testing"
 
 func TestRun(t *testing.T) {
-	results := Run([]string{"weixincloud.nbcb.com.cn"}, 500, 50, "")
+	results := Run([]string{"weixincloud.nbcb.com.cn"}, 1, 50, "")
 	t.Logf("结果数量: %v", len(results))
 	t.Log(results)
 }
